03b4aca5ddf0d83d65afee1c0d156097d426b972
N.Hugo <neonhugo@gmail.com>
2019-12-31 11:55:19 -0300

    Initial commit

---
+++ infrastructure/src/main/java/br/com/tw/infrastructure/Song.kt
@@ -0,0 +1,13 @@
+package br.com.tw.infrastructure
+
+/**
+ * Copyright 2019 Rede S.A.
+ * ************************************************************
+ * Autor    : Nalmir Hugo de A. e Silva
+ * Data     : 28/12/2019
+ * Empresa  : Iteris
+ */
+open class Song(
+    var id: Long = -1L,
+    val name: String = ""
+)
